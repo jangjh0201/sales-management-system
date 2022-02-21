a17dad8c75344518a629ed9ebdc15a8d83d020df
ShinHyeong <limo730@naver.com>
2022-02-21 19:53:03 +0900

    feat: purchaseByStore jsGrid 요소 중 매입상품 제거

--- src/main/resources/static/js/purchaseByStore_B2B.js
+++ src/main/resources/static/js/purchaseByStore_B2B.js
@@ -1,14 +1,8 @@
 var clients = [
-  { "product_B2B": 2, "store": 1, "purchase": 2000000},
-  { "product_B2B": 1, "store": 2, "purchase": 1000000},
+  {"store": 1, "purchase": 2000000},
+  {"store": 2, "purchase": 1000000},
 ];
 
-var products_B2B = [
-  { Name: "", Id: 0 },
-  { Name: "구연산나트륨", Id: 1 },
-  { Name: "수크랄로스", Id: 2 },
-  { Name: "아스파탐", Id: 3 }
-];
 
 var stores = [
   { Name: "", Id: 0 },
@@ -46,7 +40,6 @@ $("#jsGrid").jsGrid({
   data: clients,
 
   fields: [
-      { name: "product_B2B", type: "select", title:"매입상품", width: 50, items: products_B2B, valueField: "Id", textField: "Name" },
       { name: "store", type: "select", title:"매출처", width: 50, items: stores, valueField: "Id", textField: "Name" },
       { name: "purchase", type: "number", title:"매입액", width: 50 },
       { type: "control" }
